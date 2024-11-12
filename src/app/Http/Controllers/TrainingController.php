c337d896a8dda8259ff02b7826ddfe5f8c241a9b
crazymaki <katami4koji.923@gmail.com>
2024-11-13 02:22:04 +0900

    Training画面の初期表示

--- src/app/Http/Controllers/TrainingController.php
+++ src/app/Http/Controllers/TrainingController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ExercisesCategoryMaster;
 
 class TrainingController extends Controller
 {
     public function index()
     {
-        return view('training.index');
+        $categories = ExercisesCategoryMaster::all();
+
+        return view('training.index', compact('categories'));
     }
 }
