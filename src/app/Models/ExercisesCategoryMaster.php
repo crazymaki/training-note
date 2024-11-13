f12bfbb4e7bcc43799f5bc9e01a6f2ad147fb89a
crazymaki <katami4koji.923@gmail.com>
2024-11-14 01:58:36 +0900

    不要な処理を削除

--- src/app/Models/ExercisesCategoryMaster.php
+++ src/app/Models/ExercisesCategoryMaster.php
@@ -9,21 +9,4 @@ class ExercisesCategoryMaster extends Model
 {
     use HasFactory;
 
-
-    protected $fillable = ['category_id','category_name'];
-
-    /**
-     * APIで取得データを保存
-     *
-     * @param array $apiData
-     * @return void
-     */
-    public static function saveFromApi($apiData)
-    {
-        // データベースに保存
-        return self::updateOrCreate(
-            ['category_id' => $apiData['id']],
-            ['category_name' => $apiData['name']],
-        );
-    }
 }
--- src/app/Models/ExercisesMaster.php
+++ src/app/Models/ExercisesMaster.php
@@ -9,23 +9,4 @@ class ExercisesMaster extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['id','category_id','exercise_name'];
-
-    /**
-     * APIで取得データを保存
-     *
-     * @param array $apiData
-     * @return void
-     */
-    public static function saveFromApi($apiData)
-    {
-        // データベースに保存
-        return self::updateOrCreate(
-            ['id' => $apiData['id']],
-            [
-                'category_id' => $apiData['category'],
-                'exercise_name' => $apiData['name'],
-            ]
-        );
-    }
 }
